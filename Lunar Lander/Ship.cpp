c524cf3541b3d22b006642fe8990bacd0d02a748
ylime11 <ylime11@live.com>
2013-12-06 16:38:14 -0600

    Update Ship.cpp

--- Lunar Lander/Ship.cpp
+++ Lunar Lander/Ship.cpp
@@ -1,37 +1,63 @@
 #include "Ship.h"
-#include <fstream>
-#include "Plotter.h"
+
 
 
 Ship::Ship()
 {
+    rocket[0][0] = black;
+    rocket[0][1] = black;
+    rocket[0][2] = green;
+    rocket[0][3] = black;
+    rocket[0][4] = black;
+
+    rocket[1][0] = green;
+    rocket[1][1] = green;
+    rocket[1][2] = green;
+    rocket[1][3] = green;
+    rocket[1][4] = green;
+
+    rocket[2][0] = green;
+    rocket[2][1] = yellow;
+    rocket[2][2] = green;
+    rocket[2][3] = yellow;
+    rocket[2][4] = green;
+
+    rocket[3][0] = yellow;
+    rocket[3][1] = green;
+    rocket[3][2] = yellow;
+    rocket[3][3] = green;
+    rocket[3][4] = yellow;
+
+    rocket[4][0] = green;
+    rocket[4][1] = black;
+    rocket[4][2] = black;
+    rocket[4][3] = black;
+    rocket[4][4] = green;
 }
 
 
 Ship::~Ship()
 {
+
 }
 
-void Ship::Draw(double deltaTime)
+void Ship::Draw(double x, double y)
 {
     // draw ship
-    
-    
-    ink rocket[5][5] = {{BLACK, BLACK, GREEN, BLACK, BLACK}, 
-                        {BLACK, BLACK, GREEN, BLACK, BLACK}, 
-                        {GREEN, YELLOW, GREEN, YELLOW, GREEN}, 
-                        {YELLOW, GREEN, YELLOW, GREEN, YELLOW}, 
-                        {GREEN, BLACK, BLACK, BLACK, GREEN}};
-    
-    /*for (int r = 0; r < 5; r++)
+    Plotter p;
+
+
+
+
+    for (int r = 0; r < 5; r++)
     {
         for (int c = 0; c < 5; c++)
         {
-            file >> pixel;
-            rocket[r][c] = pixel;
+            p.setColor(rocket[r][c]);
+            p.plot(x + c, y + r, SQUARE);
         }
-    }*/
-    
+    }
+
 }
 
 void Ship::Update(double deltaTime)
